d4aa09c036330e19817ceef40aa9134fef338a58
Prashant Shakya <prashushakya@gmail.com>
2019-09-07 14:22:46 +0530

    Added client.handlers.WelcomeViewThread

---
+++ src/client/handlers/WelcomeViewThread.java
@@ -0,0 +1,21 @@
+package client.handlers;
+
+import client.Client;
+
+public class WelcomeViewThread extends Thread{
+    private final String message;
+    private final Client client;
+
+    public WelcomeViewThread(Client client, String message) {
+
+        this.message = message;
+        this.client = client;
+    }
+    /**
+     * Sends the message from the WelcomeView to the server.
+     */
+    public void run() {
+        System.out.println("sending message");
+        client.sendMessageToServer(message);
+    }
+}
