e2fca2923d5e80e486429fa4ea097c7b39daa89c
Prashant Shakya <prashushakya@gmail.com>
2019-09-07 14:19:31 +0530

    Created ConnectServer View and its controller

---
+++ src/client/controllers/ConnectServer.java
@@ -0,0 +1,78 @@
+package client.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import client.Client;
+import client.SceneController;
+import client.handlers.ConnectServerThread;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ConnectServer {
+    @FXML
+    JFXButton connectButton;
+    @FXML
+    JFXTextField serverIPfield,portfield;
+
+    Socket s;
+    private Client client;
+    private SceneController sceneController;
+    private boolean DEBUG = true;
+
+    @FXML
+    private void onClickConnect(ActionEvent event) throws IOException {
+        System.out.println("Connect Button Clicked");
+        String hostInput = serverIPfield.getText().trim();
+        String portInput = portfield.getText().trim();
+        String portRegex = "\\d\\d?\\d?\\d?\\d?";
+        if(hostInput.length() != 0 && portInput.matches(portRegex)) {
+            try {
+                client = new Client(Integer.parseInt(portInput),hostInput,sceneController);
+                sceneController.setClient(client);
+            }
+            catch (NumberFormatException e1) {
+                Alert a1 = new Alert(Alert.AlertType.ERROR);
+                a1.setTitle("Invalid Arguments");
+                a1.setContentText("Invalid Arguments");
+                a1.setHeaderText(null);
+                a1.showAndWait();
+
+            }
+            client.setSceneController(sceneController);
+            if(DEBUG) {
+                System.out.println("set client scenechanger connectserver");
+            }
+
+//            ConnectServerThread thread = new ConnectServerThread(this);
+//            thread.start();
+
+            //Have to Change this After
+            try {
+                client.start();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        else {
+            Alert a1 = new Alert(Alert.AlertType.ERROR);
+            a1.setTitle("Invalid Arguments");
+            a1.setContentText("Invalid Arguments");
+            a1.setHeaderText(null);
+            a1.showAndWait();
+        }
+
+    }
+    public void setSceneController(SceneController sceneController) {
+        this.sceneController = sceneController;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+}
