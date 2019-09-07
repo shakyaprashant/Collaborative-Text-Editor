009107f2f047ac31a98311650e5d525626d21aff
Prashant Shakya <prashushakya@gmail.com>
2019-09-07 14:23:25 +0530

    added LineNumberingTextArea

---
+++ src/client/utility/LineNumberingTextArea.java
@@ -0,0 +1,42 @@
+package client.utility;
+
+import javax.swing.*;
+import javax.swing.text.Element;
+import java.awt.*;
+
+public class LineNumberingTextArea extends JTextArea {
+    private JTextArea textArea;
+
+    public LineNumberingTextArea(JTextArea textArea)
+    {
+        this.textArea = textArea;
+        setBackground(Color.DARK_GRAY);
+        setForeground(Color.lightGray);
+        Font font = new Font("Serif", Font.PLAIN, 18);
+        setFont(font);
+        setEditable(false);
+
+    }
+
+    public void updateLineNumbers()
+    {
+        String lineNumbersText = getLineNumbersText();
+        setText(lineNumbersText);
+
+    }
+
+    private String getLineNumbersText()
+    {
+        int caretPosition = textArea.getDocument().getLength();
+        Element root = textArea.getDocument().getDefaultRootElement();
+        StringBuilder lineNumbersTextBuilder = new StringBuilder();
+        lineNumbersTextBuilder.append("1").append(System.lineSeparator());
+
+        for (int elementIndex = 2; elementIndex < root.getElementIndex(caretPosition) + 2; elementIndex++)
+        {
+            lineNumbersTextBuilder.append(elementIndex).append(System.lineSeparator());
+        }
+
+        return lineNumbersTextBuilder.toString();
+    }
+}
